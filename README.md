13ea0a483a48283ad12badc27e2364f28a446acc
maulidkategile <maulidkategile@gmail.com>
2021-01-02 11:48:00 +0300

    initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# bloc_pattern_blog
+
+A flutter application that uses strems for state management with BLoC pattern
---
+++ android/app/src/main/kotlin/com/example/bloc_pattern_blog/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.bloc_pattern_blog
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
